04dbc9447d66f5a0564ef72f495af8f1aa0f461c
Username4Fabian <03anion.proxys@icloud.com>
2023-09-22 08:56:50 +0200

    Added Project Structure

---
+++ src/test/java/htlle/mailresponse/MailresponseApplicationTests.java
@@ -0,0 +1,13 @@
+package htlle.mailresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailresponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
