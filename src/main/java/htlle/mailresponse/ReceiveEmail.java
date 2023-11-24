6b3a65ef49055e0080932dd240ddecbdd96713c4
PDPinska <191witb16@o365.htl-leoben.at>
2023-11-24 08:39:20 +0100

    Sending Emails

---
+++ src/main/java/htlle/mailresponse/ReceiveEmail.java
@@ -0,0 +1,46 @@
+package htlle.mailresponse;
+
+
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class ReceiveEmail {
+
+    /**
+     Outgoing Mail (SMTP) Server
+     requires TLS or SSL: smtp.gmail.com (use authentication)
+     Use Authentication: Yes
+     Port for TLS/STARTTLS: 587
+     */
+
+
+    public static void main(String[] args) {
+        final String fromEmail = "bikebuilder03@outlook.com"; //requires valid gmail id
+        final String password = "hutter1234"; // correct password for gmail id
+        final String toEmail = "191witb16@o365.htl-leoben.at"; // can be any email id
+
+        System.out.println("TLSEmail Start");
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp-mail.outlook.com"); //SMTP Host
+        props.put("mail.smtp.port", "587"); //TLS Port
+        props.put("mail.smtp.auth", "true"); //enable authentication
+        props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, password);
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+
+        EmailUtil.sendEmail(session, toEmail, fromEmail, "TLSEmail Testing Subject", "TLSEmail Testing Body");
+
+    }
+
+
+}
